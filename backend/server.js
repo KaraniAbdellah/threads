a4d8403c087c749e77602517209a6effd74bdd74
KaraniAbdellah <abdellahkarani@gmail.com>
2025-04-22 23:23:40 +0100

    make server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,11 @@
-// Start Backend Here
+import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+const server = express();
 
 
+server.listen(process.env.PORT, (err) => {
+    err ? console.log(`Error While Starting The Server ${err}`) : console.log(`Server Running In Port ${process.env.PORT}`);
+});
 
