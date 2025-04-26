6da2d9e4840b7a4bbb91f1ff0573ffe70f59c7df
KaraniAbdellah <abdellahkarani@gmail.com>
2025-04-27 00:55:57 +0100

    add get_user_profile end Point

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -4,8 +4,29 @@ import UserModel from "../models/User.js";
 import bcrypt, { hash } from "bcrypt";
 
 
-const test = (req, res) => {
-    res.json("Hello Test");
+const get_user_profile = async (req, res) => {
+    try {
+        const {username} = req.params.username;
+        const user = await UserModel.findOne({user_name: username}).select("-password");
+        if (!user) return res.status(404).send({message: "User Not Found"});
+        res.status(200).send(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send({error: error.message});
+    }
+}
+const get_suggested_users = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+const follow_unfollow = async (req, res) => {
+    
+}
+const update_user_profile = async (req, res) => {
+    
 }
 
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import protect_middlware from "../middlwares/protect_middlware.js";
-import { get_user_profile, get_suggested_users, follow_unfollow, update_user_profile } from "../controllers/user.controllers.js";
-
+import {
+  get_user_profile,
+  get_suggested_users,
+  follow_unfollow,
+  update_user_profile,
+} from "../controllers/user.controllers.js";
 
 const userRouter = express.Router();
-userRouter.get("/profile/:username", get_user_profile);
-userRouter.get("/suggested", get_suggested_users);
-userRouter.get("/follow/:id", follow_unfollow);
-userRouter.get("/profile/:username", update_user_profile);
-
-
-
-
+userRouter.get("/profile/:username", protect_middlware, get_user_profile);
+userRouter.get("/suggested", protect_middlware, get_suggested_users);
+userRouter.get("/follow/:id", protect_middlware, follow_unfollow);
+userRouter.get("/profile/:username", protect_middlware, update_user_profile);
 
 export default userRouter;
