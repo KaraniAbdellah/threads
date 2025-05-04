f504a654b6c141f2e7fdd9851ee74c3f54057494
KaraniAbdellah <abdellahkarani@gmail.com>
2025-05-05 00:13:43 +0100

    fix social compoents

--- frontend/src/components/Social_media.jsx
+++ frontend/src/components/Social_media.jsx
@@ -4,46 +4,18 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { IoPersonCircleOutline } from "react-icons/io5";
 
-
 export default function SocialMedia() {
   return (
     <div>
-  <button class="social_media">
-      <div class="icon">
-        <FaShare className="shere"/>
-          <FaLinkedin className="icon-shere"/>
-        <FaGithub className="icon-shere"/>
-        <IoPersonCircleOutline className="icon-shere"/>
-      </div>
-      <p>Share me</p>
-  </button>
+      <button class="social_media z-10 px-4 py-2 border border-sky-600 rounded hover:bg-sky-800" >
+        <div class="icon">
+          <FaShare className="shere" />
+          <FaLinkedin className="icon-shere" />
+          <FaGithub className="icon-shere" />
+          <IoPersonCircleOutline className="icon-shere" />
+        </div>
+        <p className="ml-8">Share me</p>
+      </button>
     </div>
   );
-};
-
-// export default SocialMedia;
-// import React from "react";
-// import { FaLinkedin } from "react-icons/fa";
-// import { BsTwitterX } from "react-icons/bs";
-
-
-
-// const SocialMedia = () => {
-//   return (
-//     <div>
-//       <button class="social_media">
-//         <div class="icon">
-//         <CiShare2 />
-
-//           <FaLinkedin />
-//           <FaGithub />
-//           <IoPersonCircleOutline />
-//           <BsTwitterX />
-//         </div>
-//         <p>Share me</p>
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default SocialMedia;
+}
