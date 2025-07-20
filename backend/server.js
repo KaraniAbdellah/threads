8642f10604de9bdd604d5942df9839350bc7379d
KaraniAbdellah <abdellahkarani@gmail.com>
2025-07-20 19:18:06 +0100

    add new endpoint for get

--- backend/server.js
+++ backend/server.js
@@ -3,44 +3,44 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import cors from "cors";
 
-
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import postRouter from "./routes/post.routes.js";
-import notificationRouter from './routes/notification.routes.js';
+import notificationRouter from "./routes/notification.routes.js";
 
 import { connectDb } from "./config/db.js";
 import cookieParser from "cookie-parser";
-import {v2 as cloudinary} from "cloudinary";
-
+import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_COULD,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SCRET_KEY,
-    // secure: true
+  cloud_name: process.env.CLOUDINARY_COULD,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SCRET_KEY,
+  // secure: true
 });
 
 const server = express();
 // Allows cookies and credentials and Allow request from any origin
-server.use(cors({
+server.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
-server.use(express.json({limit: "10mb"}));
+  })
+);
+server.use(express.json({ limit: "10mb" }));
 server.use(cookieParser());
 server.use("/api/auth", authRouter);
 server.use("/api/user", userRouter);
 server.use("/api/post", postRouter);
 server.use("/api/notification", notificationRouter);
 
-
-
-
+server.get("/", function () {
+  console.log("Hello World");
+});
 
 server.listen(process.env.PORT, (err) => {
-    err
-        ? console.log(`Error While Starting The Server ${err}`)
-        : console.log(`Server Running In Port ${process.env.PORT}`);
-    connectDb();
+  err
+    ? console.log(`Error While Starting The Server ${err}`)
+    : console.log(`Server Running In Port ${process.env.PORT}`);
+  connectDb();
 });
